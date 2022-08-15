551a5f63b36188b02269fbded454fac6ffd7a9d8
Dinuka Wanniarachchi <dinukaw95@gmail.com>
2022-08-15 19:40:25 +0530

    add nginx conf file and php script

---
+++ app/index.php
@@ -0,0 +1,13 @@
+<!Doctype html>
+<html>
+    <head>
+        <title> Hello World! </title>
+    </head>
+
+    <body>
+        <?php
+            echo "Hello World";
+        ?>
+            
+    </body>
+</html>
